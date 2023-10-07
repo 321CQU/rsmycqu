d27bd43221f0306e066747b0244d669d60f9481f
Zhu Zijun <2961163526@qq.com>
2023-10-08 02:29:36 +0800

    增加了查询用户信息的方法
    Signed-off-by: Zhu Zijun <2961163526@qq.com>

--- src/mycqu/mod.rs
+++ src/mycqu/mod.rs
@@ -1,5 +1,8 @@
 //! 提供教务网`my.cqu.edu.cn`的已知可用接口
 
+use reqwest::StatusCode;
+use serde::{Serialize, Deserialize};
+
 use crate::errors::mycqu::MyCQUResult;
 use crate::errors::{Error, ErrorHandler};
 use crate::mycqu::utils::access::get_oauth_token;
@@ -7,6 +10,8 @@ use crate::session::access_info::{AccessInfoValue, MYCQU_ACCESS_INFO_KEY};
 use crate::session::Session;
 use crate::sso::access_services;
 use crate::utils::consts::MYCQU_SERVICE_URL;
+
+use crate::utils::APIModel;
 
 mod utils;
 
@@ -34,4 +39,41 @@ pub async fn access_mycqu(session: &mut Session) -> MyCQUResult<()> {
         },
     );
     Ok(())
+}
+
+/// 教务网用户信息接口响应数据模型
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct User {
+    /// 姓名
+    pub name: String,
+    /// 统一身份认证号
+    #[serde(alias="username")]
+    pub id: String,
+    /// 学工号
+    pub code: String,
+    /// 身份，已知取值有学生(`student`)、教师(`instructor`)
+    #[serde(alias="type")]
+    pub role: String,
+    /// 电子邮箱
+    pub email: Option<String>,
+    /// 电话号码
+    #[serde(alias="phoneNumber")]
+    pub phone_number: Option<String>
+}
+
+impl APIModel for User{}
+
+impl User {
+    pub async fn call(session: &Session) -> MyCQUResult<Self> {
+        if !session.can_access(&MYCQU_ACCESS_INFO_KEY) {
+            return Err(Error::NotAccess)
+        }
+
+        let res = session.client.get("https://my.cqu.edu.cn/authserver/simple-user").send().await?;
+
+        if res.status() == StatusCode::UNAUTHORIZED {
+            return Err(Error::NotAccess)
+        }
+        Ok(res.json::<Self>().await?)
+    }
 }
--- src/mycqu/tests/mod.rs
+++ src/mycqu/tests/mod.rs
@@ -1,7 +1,7 @@
-use crate::mycqu::access_mycqu;
+use crate::mycqu::{access_mycqu, User};
 use crate::session::access_info::MYCQU_ACCESS_INFO_KEY;
 use crate::session::Session;
-use crate::utils::test_fixture::login_session;
+use crate::utils::test_fixture::{access_mycqu_session, login_session};
 use rstest::*;
 
 #[rstest]
@@ -11,4 +11,11 @@ async fn test_access_mycqu(#[future] login_session: Session) {
     let mut session = login_session.await.clone();
     access_mycqu(&mut session).await.unwrap();
     assert!(session.access_info.contains_key(&MYCQU_ACCESS_INFO_KEY));
+}
+
+#[rstest]
+#[ignore]
+#[tokio::test]
+async fn test_get_user(#[future] access_mycqu_session: Session) {
+    println!("{:?}", User::call(&access_mycqu_session.await).await.unwrap());
 }
